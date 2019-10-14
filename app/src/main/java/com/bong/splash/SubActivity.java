6d923ea832c02a7dc5d505e2ae4e5a6ae0ae7305
leebonghee <s2cronoss@gmail.com>
2019-10-14 19:26:04 +0900

    add button

---
+++ app/src/main/java/com/bong/splash/SubActivity.java
@@ -0,0 +1,15 @@
+package com.bong.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+    }
+}
