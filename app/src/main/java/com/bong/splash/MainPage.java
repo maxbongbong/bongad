80317c28e7964d39d1408648093b3325f1b6d090
leebonghee <s2cronoss@gmail.com>
2019-10-14 17:43:40 +0900

    add splash

--- app/src/main/java/com/bong/splash/MainPage.java
+++ app/src/main/java/com/bong/splash/MainPage.java
@@ -1,14 +1,15 @@
 package com.bong.splash;
 
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainPage extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1 = (Button) findViewById(R.id.button1) ;
     }
 }
 
