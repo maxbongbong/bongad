4de29c8453790e23ff89fcd12316b213edcfc6b9
leebonghee <s2cronoss@gmail.com>
2019-10-15 18:33:04 +0900

    add subpage

--- app/src/main/java/com/bong/splash/SubActivity.java
+++ app/src/main/java/com/bong/splash/SubActivity.java
@@ -2,7 +2,6 @@ package com.bong.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class SubActivity extends AppCompatActivity {
 
