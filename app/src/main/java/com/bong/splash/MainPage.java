2338342e6913df759440f4f6204ae65baddacead
leebonghee <s2cronoss@gmail.com>
2019-10-15 17:04:34 +0900

    add subpage

--- app/src/main/java/com/bong/splash/MainPage.java
+++ app/src/main/java/com/bong/splash/MainPage.java
@@ -1,7 +1,9 @@
 package com.bong.splash;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainPage extends Activity {
@@ -9,6 +11,14 @@ public class MainPage extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
