19bd2365895f78576b57e6dbb82029e520d63bec
leebonghee <s2cronoss@gmail.com>
2019-10-19 17:47:27 +0900

    Add RxJava

--- app/src/main/java/com/bong/splash/Apiservice.java
+++ app/src/main/java/com/bong/splash/Apiservice.java
@@ -2,12 +2,9 @@ package com.bong.splash;
 
 import com.bong.splash.data.Lotto;
 
-import okhttp3.ResponseBody;
+import io.reactivex.Single;
 import retrofit2.Call;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface Apiservice {
@@ -15,4 +12,7 @@ public interface Apiservice {
 
     @GET ("common.do?method=getLottoNumber")
     Call<Lotto>getComment(@Query("drwNo") int drwNo);
+
+    @GET ("common.do?method=getLottoNumber")
+    Single<Lotto> getCommentRx(@Query("drwNo") int drwNo);
 }
